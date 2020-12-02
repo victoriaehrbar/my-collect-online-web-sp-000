47960ede69fc13b5916b9f63c4b0a626b81cfb1b
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:21:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require "pry"
 
-
+def my_collect(collection)
+# binding.pry
+counter = 0
+collect = []
+while counter < collection.length
+collect << yield(collection[counter])
+  counter += 1
+end
+collect
+end
