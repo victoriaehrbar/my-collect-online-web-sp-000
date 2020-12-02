db27aa5f42ae680ed7e6831f3cd2a0585ac4ec22
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:12:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ require "pry"
 def my_collect(collection)
 binding.pry
 counter = 0
-while counter < 
+while counter < collection.length
   puts "The current number is less than 20."
   counter += 1
 end
