c553479f92d6607ed51029f0c43e2995937d760b
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:19:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 require "pry"
 
 def my_collect(collection)
-binding.pry
+# binding.pry
 counter = 0
 collect = []
 while counter < collection.length
-collection << yield(collection[counter]) 
+collection << yield(collection[counter])
   counter += 1
 end
 
